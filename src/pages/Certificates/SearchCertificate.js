5b581cb67ab0e1c1ce104d62aba3e8387b2f5257
iceyisaak <me@iceyisaak.com>
2022-02-07 11:11:59 +0100

    Added searchTerm

--- src/pages/Certificates/SearchCertificate.js
+++ src/pages/Certificates/SearchCertificate.js
@@ -1,34 +1,49 @@
-import React, { useState } from 'react';
+import { Contract } from 'near-api-js';
+import React, { useEffect, useState } from 'react';
 
 const SearchCertificate = (props) => {
 
   const { certificateList, personList, vaccineList } = props;
   const [searchResult, setSearchResult] = useState({});
+  const [searchTerm, setSearchTerm] = useState('');
 
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    try {
-      await window.contract.getCertificateByPersonID({
-        id: id.value,
-      });
-      // HOW TO RETRIEVE RESULT FROM FUNCTION?
-      setSearchResult(personCertificate);
-    } catch (e) {
-      alert(
-        'Something went wrong! ' +
-        'Maybe you need to sign out and back in? ' +
-        'Check your browser console for more info.'
-      );
-      throw e;
-    } finally {
-      // fieldset.disabled = false;
-    }
+    // try {
+    //   await window.contract.getCertificateByPersonID({
+    //     id: id.value,
+    //   });
+    //   // HOW TO RETRIEVE RESULT FROM FUNCTION?
+    //   setSearchResult(personCertificate);
+    // } catch (e) {
+    //   alert(
+    //     'Something went wrong! ' +
+    //     'Maybe you need to sign out and back in? ' +
+    //     'Check your browser console for more info.'
+    //   );
+    //   throw e;
+    // } finally {
+    //   // fieldset.disabled = false;
+    // }
     // alert('New Certificate Added');
     // location.assign('/certificates');
 
+    setSearchTerm(e.target.value);
     console.log(searchResult);
   };
+
+
+  const fetchCertificateByPersonID = async (searchTerm) => {
+    const res = await contract.getCertificateByPersonID({ id: id.value });
+    setSearchResult(res);
+  };
+
+  useEffect(() => {
+    fetchCertificateByPersonID();
+  }, [searchTerm]);
+
+
 
 
   return (
