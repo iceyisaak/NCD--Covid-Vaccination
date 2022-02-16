41d326f7d9da7ca9959b561ca4212a9ca07c228d
iceyisaak <me@iceyisaak.com>
2022-02-16 15:29:00 +0100

    Resolving SearchCertificateByCountry

--- src/pages/Certificates/SearchCertificateByCountry.js
+++ src/pages/Certificates/SearchCertificateByCountry.js
@@ -3,10 +3,12 @@ import { useNavigate } from 'react-router-dom';
 
 const SearchCertificateByCountry = (props) => {
 
-  const { certificateList, personList, vaccineList } = props;
+  // const { certificateList, personList, vaccineList } = props;
+  const [certificateList, setCertificateList] = useState([]);
   const [searchResult, setSearchResult] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [showResult, setShowResult] = useState(false);
+  const [country, setCountry] = useState([]);
 
   const navigate = useNavigate();
 
@@ -28,30 +30,57 @@ const SearchCertificateByCountry = (props) => {
   };
 
 
+
+  const getUnique = (arr, index) => {
+
+    const unique = arr
+      .map(e => e[index])
+
+      // store the keys of the unique objects
+      .map((e, i, final) => final.indexOf(e) === i && i)
+
+      // eliminate the dead keys & store unique objects
+      .filter(e => arr[e]).map(e => arr[e]);
+
+    return unique;
+  };
+
+  useEffect(() => {
+    contract.getCertificates().then(setCertificateList);
+    // const uniqueSet = new Set(ce);
+    console.log('certificateList: ', certificateList);
+  }, []);
+
+
   useEffect(() => {
     fetchCertificateByCountry(searchTerm);
   }, [searchTerm]);
 
-  console.log('searchResult: ', searchResult);
+  // const countries = certificateList.map((c) => c.country);
+  // const uniqueCountry = getUnique(countries, certificateList.id);
+  // console.log('uniqueSet: ', uniqueSet);
+  // setCountry(uniqueSet);
+  // console.log('country: ', country);
+  // console.log('certificateList: ', certificateList);
+  // console.log('uniqueCountry: ', uniqueCountry);
 
   return (
     <>
+
       <h1>Search Certificate by Country</h1>
       <form onSubmit={onSubmit} className='searchBox'>
         <label htmlFor="country">Search Country</label>
         <select name="country" id="country">
-          {certificateList.map((c, i) => <option value={c.country} key={i}>{c.country}</option>)}
+          {/* {uniqueSet.map((c, i) => <option value={c.country} key={i}>{c.country}</option>)} */}
         </select>
         <button>Submit</button>
         <button onClick={() => navigate('/certificates')}>Back</button>
       </form>
-
       {showResult ?
         searchResult.length === 0 ?
           <p>No Vaccine Certificate</p> :
           <table className='table'>
             <tbody>
-
               {searchResult.map((sR, i) =>
                 <tr key={i}>
                   <td>
