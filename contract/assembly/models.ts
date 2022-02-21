a7463aed656440c156407071e907b0cc8fdd1d80
iceyisaak <me@iceyisaak.com>
2022-02-21 23:19:32 +0100

    Updated Model.ts

--- contract/assembly/models.ts
+++ contract/assembly/models.ts
@@ -7,13 +7,26 @@ export class Vaccine {
   name: string; //Pfzer
   manufacturer: string; //BioNTech, Fosun Pharma, Pfizer
   type: string; //mRNA
-  constructor(id: string, name: string, manufacturer: string, type: string, administration: string, dose: string) {
+  constructor(id: string, name: string, manufacturer: string, type: string) {
     this.id = id;
     this.name = name;
     this.manufacturer = manufacturer;
     this.type = type;
-    this.administration = administration;
-    this.dose = dose;
+  }
+}
+
+// Exporting the VaccinationSite class
+@nearBindgen
+export class VaccinationSite {
+  id: string;
+  name: string;
+  address: string;
+  type: string;
+  constructor(id: string, name: string, address: string, type: string) {
+    this.id = id;
+    this.name = name;
+    this.address = address;
+    this.type = type;
   }
 }
 
@@ -23,32 +36,38 @@ export class Person {
   id: string;
   nationality: string;
   name: string;
-  photo: string;
+  email: string;
+  phone: string;
+  address: string;
   birthdate: string;
-  constructor(id: string, nationality: string, name: string, photo: string, birthdate: string) {
+  citizen_id: string; // Passport ID / Citizen ID / etc.  
+  constructor(id: string, nationality: string, name: string, email: string, phone: string, address: string, birthdate: string, citizen_id: string) {
     this.id = id;
     this.nationality = nationality;
     this.name = name;
-    this.photo = photo;
+    this.email = email;
+    this.phone = phone;
+    this.address = address;
     this.birthdate = birthdate;
+    this.citizen_id = citizen_id;
   }
 }
 
 // Exporting the Certificate class
 @nearBindgen
-export class Certificate {
+export class Transaction {
   id: string;
   vaccine_id: string;
   person_id: string;
-  country: string;
+  vaccination_site_id: string
   application_date: string;
   vaccine_lot: string;
   digital_stamp: string;
-  constructor(id: string, vaccine_id: string, person_id: string, country: string, application_date: string, vaccine_lot: string, digital_stamp: string) {
+  constructor(id: string, vaccine_id: string, person_id: string, vaccination_site_id: string, application_date: string, vaccine_lot: string, digital_stamp: string) {
     this.id = id;
     this.vaccine_id = vaccine_id;
     this.person_id = person_id;
-    this.country = country;
+    this.vaccination_site_id = vaccination_site_id;
     this.application_date = application_date;
     this.vaccine_lot = vaccine_lot;
     this.digital_stamp = digital_stamp;
@@ -56,5 +75,6 @@ export class Certificate {
 }
 
 export const vaccines = new PersistentVector<Vaccine>("v");
+export const vaccination_sites = new PersistentVector<VaccinationSite>("vs");
 export const persons = new PersistentVector<Person>("p");
-export const certificates = new PersistentVector<Certificate>("c");
+export const transactions = new PersistentVector<Transaction>("t");
