b19516586bfc82ac5b896e4279d51677cfb6ea1c
iceyisaak <me@iceyisaak.com>
2022-02-26 15:36:15 +0100

    Resolving TransactionList

--- src/pages/Transactions/TransactionList.jsx
+++ src/pages/Transactions/TransactionList.jsx
@@ -8,56 +8,35 @@ const TransactionList = (props) => {
 
   console.log('transactionList-1: ', transactionList);
 
-
-  if (transactionList) {
-
-    const res = contract.getTransactionByPersonID({ id: transactionList });
+  const fetchTransactions = async () => {
+    const res = await contract.getTransactions({ id: transactionList });
     if (res) {
-
-
       for (let i = 0; i < res.length; i++) {
-        console.log('res: ', res);
 
-        let vaccine = contract.getVaccineByID({ id: res[i].vaccine_id });
+        let vaccine = await contract.getVaccineByID({ id: res[i].vaccine_id });
         console.log('vaccine: ', vaccine);
         res[i].vaccine_name = vaccine[0].name;
 
-        let vaccination_site = contract.getVaccinationSiteByID({ id: res[i].vaccination_site_id });
+        let vaccination_site = await contract.getVaccinationSiteByID({ id: res[i].vaccination_site_id });
         console.log('vaccination_site: ', vaccination_site);
         res[i].vaccination_site_name = vaccination_site[0].name;
 
-        let person = contract.getPersonByID({ id: res[i].person_id });
+        let person = await contract.getPersonByID({ id: res[i].person_id });
         console.log('person: ', person);
         res[i].person_name = person[0].name;
       }
       setShowData(res);
+    } else {
+      setShowData(null);
     }
-  }
+  };
 
   console.log('showData: ', showData);
 
-  // const fetchTransactions = async (transactionList) => {
 
-  //   console.log('fetchTransactions() ');
-  //   console.log('transactionList[0].person_id: ', transactionList);
-  //   // if (searchTerm !== '') {
-  //   const res = await contract.getPersonByID({ id: transactionList.person_id });
-  //   if (res) {
-  //     console.log('res: ', res);
-  //     // for (let i = 0; i < res.length; i++) {
-
-
-  //     // }
-  //     //     setSearchResult(res);
-  //     //   } else {
-  //     //     setSearchResult(null);
-  //     //   }
-  //   };
-  // };
-
-  // useEffect(() => {
-  //   fetchTransactions();
-  // }, []);
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
 
   return (
     <div className='list'>
@@ -80,26 +59,25 @@ const TransactionList = (props) => {
                 Vaccination Site
               </td>
             </tr>
-            {console.log('transactionList: ', transactionList),
-              transactionList.map((t, i) =>
-                <tr key={i}>
-                  <td>
-                    <Link to={`/transactions/${t.id}`}>
-                      {t.person_id}
-                    </Link>
-                  </td>
-                  <td>
-                    <Link to={`/transactions/${t.id}`}>
-                      {t.vaccine_id}
-                    </Link>
-                  </td>
-                  <td>
-                    <Link to={`/transactions/${t.id}`}>
-                      {t.vaccination_site_id}
-                    </Link>
-                  </td>
-                </tr>
-              )}
+            {transactionList.map((t, i) =>
+              <tr key={i}>
+                <td>
+                  <Link to={`/transactions/${t.id}`}>
+                    {t.person_id}
+                  </Link>
+                </td>
+                <td>
+                  <Link to={`/transactions/${t.id}`}>
+                    {t.vaccine_id}
+                  </Link>
+                </td>
+                <td>
+                  <Link to={`/transactions/${t.id}`}>
+                    {t.vaccination_site_id}
+                  </Link>
+                </td>
+              </tr>
+            )}
           </tbody >
         </table >
       }
